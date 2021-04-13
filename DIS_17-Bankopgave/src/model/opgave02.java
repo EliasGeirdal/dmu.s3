fba7531fbf49c6fa3d6ef3f3e64fe7da97bc2155
Elias <elias@geirdal.dk>
2021-04-13 13:35:53 +0200

    opgave 2 og 3

---
+++ DIS_17-Bankopgave/src/model/opgave02.java
@@ -0,0 +1,107 @@
+package model;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class opgave02 {
+	public static void main(String[] args) {
+		try {
+			Connection minConnection;
+			minConnection = DriverManager.getConnection(
+					"jdbc:sqlserver://DESKTOP-A9FL7AN\\SQLEXPRESS;databaseName=bank;user=sa;password=cookie;");
+			
+			System.out.println("Opret overførsel ");
+			BufferedReader inLine = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.print("Indtast frakonto regNr.: ");
+			String fraRegNr = "1234"; // inLine.readLine();
+			System.out.println("Indtast frakonto kontoNr.: ");
+			String fraKontoNr = "1234567"; // inLine.readLine();
+
+			System.out.print("Indtast tilkonto regNr.: ");
+			String tilRegNr = "1216";// inLine.readLine();
+			System.out.println("Indtast tilkonto kontoNr.: ");
+			String tilKontoNr = "2234567";// inLine.readLine();
+
+			System.out.println("Indtast beløb: ");
+			double beløb = 200.00;// Double.parseDouble(inLine.readLine());
+
+			String chkFraKonto = "select saldo from Konto where Konto.ktoNr = "
+					+ fraKontoNr
+					+ "and Konto.regNr = " + fraRegNr;
+
+			String chkTilKonto = "select saldo from Konto where Konto.ktoNr = " + tilKontoNr + "and Konto.regNr = "
+					+ tilRegNr;
+
+			PreparedStatement prestmt = minConnection.prepareStatement(chkFraKonto);
+			Statement stmt = minConnection.createStatement();
+
+			stmt.execute("begin tran");
+			ResultSet res = prestmt.executeQuery();
+			res.next();
+			System.out.println(res.getString(1));
+			double fraSaldo = Double.parseDouble(res.getString(1));
+			System.out.println(fraSaldo);
+			if (fraSaldo > beløb) {
+
+				System.out.println("Saldo er større end beløb");
+				prestmt = minConnection.prepareStatement(chkTilKonto);
+
+				res = prestmt.executeQuery();
+				res.next();
+				double tilSaldo = Double.parseDouble(res.getString(1));
+				System.out.println(tilSaldo);
+
+				fraSaldo -= beløb;
+				tilSaldo += beløb;
+
+				System.out.println("Fremprovokere en fejl: ");
+				inLine.readLine();
+				
+				String updateFraKonto = "update Konto set saldo = " + fraSaldo
+						+ "where Konto.ktoNr = "
+								+ fraKontoNr
+								+ "and Konto.regNr = " + fraRegNr;
+				prestmt = minConnection.prepareStatement(updateFraKonto);
+
+				prestmt.executeUpdate();
+
+				String updateTilKonto = "update Konto set saldo = " + tilSaldo
+						+ "where Konto.ktoNr = " + tilKontoNr + "and Konto.regNr = "
+								+ tilRegNr;
+				prestmt = minConnection.prepareStatement(updateTilKonto);
+
+				prestmt.executeUpdate();
+			}
+
+			prestmt = minConnection.prepareStatement(chkFraKonto);
+
+			res = prestmt.executeQuery();
+			res.next();
+			System.out.println(res.getString(1));
+
+			prestmt = minConnection.prepareStatement(chkTilKonto);
+
+			res = prestmt.executeQuery();
+			res.next();
+			System.out.println(res.getString(1));
+
+			stmt.execute("commit tran");
+
+			if (prestmt != null)
+				prestmt.close();
+			if (minConnection != null)
+				minConnection.close();
+		} catch (SQLException e) {
+			System.out.println("fejl:  " + e.getErrorCode());
+			System.out.println("fejl:  " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("fejl: " + e.getMessage());
+		}
+	}
+}
--- DIS_17-Bankopgave/src/model/opgave03.java
+++ DIS_17-Bankopgave/src/model/opgave03.java
@@ -1,10 +1,13 @@
+package model;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class TransferApp {
+public class opgave03 {
 	public static void main(String[] args) {
 		try {
 			Connection minConnection;
@@ -12,20 +15,21 @@ public class TransferApp {
 					"jdbc:sqlserver://DESKTOP-A9FL7AN\\SQLEXPRESS;databaseName=bank;user=sa;password=cookie;");
 			
 			System.out.println("Opret overførsel ");
-//			BufferedReader inLine = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader inLine = new BufferedReader(new InputStreamReader(System.in));
 
-			System.out.print("Indtast frakonto regNr.: ");
+			System.out.println("Indtast frakonto regNr.: ");
 			String fraRegNr = "1234"; // inLine.readLine();
 			System.out.println("Indtast frakonto kontoNr.: ");
 			String fraKontoNr = "1234567"; // inLine.readLine();
 
-			System.out.print("Indtast tilkonto regNr.: ");
+			System.out.println("Indtast tilkonto regNr.: ");
 			String tilRegNr = "1216";// inLine.readLine();
 			System.out.println("Indtast tilkonto kontoNr.: ");
 			String tilKontoNr = "2234567";// inLine.readLine();
 
 			System.out.println("Indtast beløb: ");
-			double beløb = 500.00;// Double.parseDouble(inLine.readLine());
+			double beløb = 200.00;// Double.parseDouble(inLine.readLine());
+			System.out.println("Beløb indtastet: " + beløb);
 
 			String chkFraKonto = "select saldo from Konto where Konto.ktoNr = "
 					+ fraKontoNr
@@ -34,55 +38,56 @@ public class TransferApp {
 			String chkTilKonto = "select saldo from Konto where Konto.ktoNr = " + tilKontoNr + "and Konto.regNr = "
 					+ tilRegNr;
 
-			PreparedStatement prestmt = minConnection.prepareStatement(chkFraKonto);
+			Statement stmt = minConnection.createStatement();
+			stmt.execute("set transaction isolation level repeatable read");
+			stmt.execute("begin tran");
 
-			ResultSet res = prestmt.executeQuery();
+			ResultSet res = stmt.executeQuery(chkFraKonto);
 			res.next();
-			System.out.println(res.getString(1));
 			double fraSaldo = Double.parseDouble(res.getString(1));
-			System.out.println(fraSaldo);
+			System.out.println("Frasaldo: " + fraSaldo);
 			if (fraSaldo > beløb) {
 				System.out.println("Saldo er større end beløb");
-				prestmt = minConnection.prepareStatement(chkTilKonto);
 
-				res = prestmt.executeQuery();
+				res = stmt.executeQuery(chkTilKonto);
 				res.next();
 				double tilSaldo = Double.parseDouble(res.getString(1));
-				System.out.println(tilSaldo);
+				System.out.println("Tilsaldo: " + tilSaldo);
 
 				fraSaldo -= beløb;
 				tilSaldo += beløb;
 
+				System.out.println("Fremprovokere en fejl: ");
+				inLine.readLine();
+				
 				String updateFraKonto = "update Konto set saldo = " + fraSaldo
 						+ "where Konto.ktoNr = "
 								+ fraKontoNr
 								+ "and Konto.regNr = " + fraRegNr;
-				prestmt = minConnection.prepareStatement(updateFraKonto);
 
-				prestmt.executeUpdate();
+				stmt.executeUpdate(updateFraKonto);
 
 				String updateTilKonto = "update Konto set saldo = " + tilSaldo
 						+ "where Konto.ktoNr = " + tilKontoNr + "and Konto.regNr = "
 								+ tilRegNr;
-				prestmt = minConnection.prepareStatement(updateTilKonto);
 
-				prestmt.executeUpdate();
+				stmt.executeUpdate(updateTilKonto);
 			}
 
-			prestmt = minConnection.prepareStatement(chkFraKonto);
-
-			res = prestmt.executeQuery();
+			// check to see if new values are correctly updated.
+			res = stmt.executeQuery(chkFraKonto);
 			res.next();
-			System.out.println(res.getString(1));
+			System.out.println("Ny fraSaldo: " + res.getString(1));
 
-			prestmt = minConnection.prepareStatement(chkTilKonto);
-
-			res = prestmt.executeQuery();
+			res = stmt.executeQuery(chkTilKonto);
 			res.next();
-			System.out.println(res.getString(1));
+			System.out.println("Ny tilSaldo: " + res.getString(1));
 
-			if (prestmt != null)
-				prestmt.close();
+			// commit transaction
+			stmt.execute("commit tran");
+
+			if (stmt != null)
+				stmt.close();
 			if (minConnection != null)
 				minConnection.close();
 		} catch (SQLException e) {
